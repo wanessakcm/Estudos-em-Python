9158d579ddb782335f2ca48d32c7e812d21f5075
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2024-01-15 18:52:35 -0300

    explo variáveis and data types

---
+++ Variaveis e tipos de dados/app.py
@@ -0,0 +1,10 @@
+nome = "Wanessa"
+idade = "29"
+gosto_de = "Programação"
+
+print (f"Meu nome é {nome}.")
+print (f"Eu gosto de {gosto_de}")
+print (f"Eu tenho {idade} anos.")
+
+print (f"{nome} gosta de {gosto_de} e tem {idade} anos.")
+
