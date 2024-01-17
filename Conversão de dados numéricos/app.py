122c3a716ac85efbb0aaf0e9771150b27bdc7f16
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2024-01-17 12:25:26 -0300

    trabalhando com números

---
+++ Conversão de dados numéricos/app.py
@@ -0,0 +1,41 @@
+num1 = 5
+num2 = 2.5
+
+print(type(num1))
+print(type(num2))
+
+a = float(num1)
+print(a)
+print(type(a))
+
+b = int(num2)
+print(b)
+print(type(b))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+print(3+5*7+3)
+print((3+5)*(7+3))
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,85,17,-55,5))
+print(min(1,85,17,-55,5))
+
+print(round(8.51))
+
+import math
+
+print(math.floor(8.2))
+print(math.ceil(8.2))
+print(math.sqrt(9))
+
+
+
+
