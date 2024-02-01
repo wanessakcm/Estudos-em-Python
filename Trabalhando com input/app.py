beeef5817987fba5dd570d2673bf26cf3aced7c9
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2024-02-01 12:19:29 -0300

    inputs

---
+++ Trabalhando com input/app.py
@@ -0,0 +1,11 @@
+# input de usuário
+nome = input ("Digite seu nome: ")
+idade = int(input (f"Qual a sua idade {nome}?"))
+
+nascimento = 2024 - idade
+
+print(f"Seu nome é {nome}, e sua idade é: {idade}")
+print(f"{nome} você nasceus em {nascimento}")
+
+
+
